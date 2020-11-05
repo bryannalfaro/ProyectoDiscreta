80d4477065ebe8efd4030b73ad3cdd881e168205
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-11-04 19:05:22 -0600

    base

--- RSA.py
+++ RSA.py
@@ -5,3 +5,26 @@ Diego Arredondo
 Oscar Saravia
 
 Catedratico > Mario Castillo '''
+
+
+bandera = True
+
+while bandera:
+
+    print("RSA - UVG - CYPHER")
+    print("Escoge una opcion: ")
+    print("1. Encriptar")
+    print("2. Desencriptar")
+    print("3 Salir de ejecucion")
+    
+    opcion = int(input())
+    
+    if(opcion==1):
+        print("Encrypt process........")
+    
+    elif(opcion==2):
+        print("Decrypt process......")
+    
+    else:
+        print("Gracias por confiar en nosotross")
+        bandera = False
